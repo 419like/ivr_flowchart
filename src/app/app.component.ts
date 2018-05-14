318f706428b0a45bdf9b01c689b45e6f86ed65de
zhang419 <419like@gmail.com>
2018-05-15 00:32:40 +0800

    add item handle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,51 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   treeData = {
   	node1:{
-  		id:'1',
+  		id:'node1',
   		name:'a1',
-  		x:'10',
-  		y:'10'
+  		x:'100',
+  		y:'0'
   	},
   	node2:{
-  		id:'2',
+  		id:'node2',
   		name:'a2',
   		x:'10',
-  		y:'20'
-  	}
+  		y:'100',
+      parentNode:'node1'
+  	}，
+    node3:{
+      id:'node3',
+      name:'a3',
+      x:'300',
+      y:'100',
+      parentNode:'node1'
+    }
   };
-  currentItem = {}
+  lineData = [{
+    start:'node1',
+    id:'1-2',
+    end:'node2',
+    text:'是'
+  },{
+    start:'node1',
+    id:'1-3',
+    end:'node3',
+    text:'否'
+  }]
+  a = 1
+  currentItem = this.treeData.node1
   objectKeys(obj) {
     return Object.keys(obj);
   }
   onSelect(item){
   	this.currentItem = item;
   	console.log(this.currentItem);
-  }
-  setPosition(item){
-  	let styleStr = 'left:'+item.x+'px;'
-  	styleStr += 'top:'+item.y+'px;'
-  	if(item==this.currentItem){
-  		styleStr += 'border:1px solid #000;'
-  	}
-  	return styleStr
   }
 }
